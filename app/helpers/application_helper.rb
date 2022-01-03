107380fd2a4d834c5c11ac6ee4cacc389ea45266
Toshiki Matsuoka <ruirairin@outlook.jp>
2022-01-03 13:52:12 +0900

    gem meta-tagsの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+        def default_meta_tags
+          {
+            title: 'Animeter',
+            reverse: true,
+            separator: '|',
+            description: 'あなたの好きなアプリを登録し、友達と共通して好きなアニメを探せるWebアプリ',
+            keywords: 'Animeter',
+            canonical: request.original_url,
+            noindex: ! Rails.env.production?,
+            og: {
+              site_name: 'Animeter',
+              title: 'Animeter あなたの好きなアニメを登録しよう！',
+              description: 'あなたの好きなアプリを登録し、友達と共通して好きなアニメを探せるWebアプリ', 
+              type: 'website',
+              url: request.original_url,
+              image: image_url('animeter-neon.jpg'),
+              locale: 'ja_JP',
+            },
+            twitter: {
+              card: 'summary_large_image',
+              site: '@haruten3200m',
+            }
+          }
+        end
 end
